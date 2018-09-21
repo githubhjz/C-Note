88c3c5f1d7525bdcce9b5d5f9b362a0d0878a843
zhanghuijun <zhanghuijun@everhigh.com.cn>
2018-09-21 16:15:04 +0800

    creat

---
+++ src/CBase64.c
@@ -0,0 +1,42 @@
+/**
+ *  Copyright (c) 2015, Everhigh Tech. Ltd. Co.
+ *  		All rights reserved
+ */
+
+/**
+ * \addtogroup Base64
+ * @{
+ */
+
+/**
+ * \file		  CBase64.c
+ * \brief		  The HG packet library for the communication
+ *
+ * \author		zhjxy <zhanghuijun@everhigh.com.cn>
+ * \version	    V0.0.1
+ * \date		2018-9-21
+ * \license
+ *------------------------------------------------------------------*
+ *	Change History :
+ *  <Date>     | <Version> | <Author>      | <Description>          *
+ *------------------------------------------------------------------*
+ *  2018/09/21 | 0.0.1   | zhjxy       | Create file            *
+ */
+#include "CBase64.h"
+
+
+
+/**
+ * \brief  					
+ * \param  
+ * \param	 
+ * \param	 
+ * \param	 
+ * \return
+ */
+void function(void)
+{
+}
+
+/** @} */
+
